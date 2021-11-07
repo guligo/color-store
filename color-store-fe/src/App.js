be416726782e5a56248bb7ead26b4eab01b569c4
guligo <igors.gulbinskis@gmail.com>
2021-11-07 13:58:39 +0100

    Initial integration with MetaMask

--- color-store-fe/src/App.js
+++ color-store-fe/src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import CircleIcon from '@mui/icons-material/Circle';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      publicKey: null,
+      colors: [],
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("load", async () => {
+      var publicKey = await window.ethereum.request({method: 'eth_accounts'});
+      fetch('http://localhost:8080/colors')
+        .then(res => {
+          return res.json();
+        })
+        .then(colors => this.setState({
+          publicKey: publicKey,
+          colors: colors
+        }))
+        .catch(console.log)
+    });
+  }
+
+  render() {
+    return (
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" component="div">
+              Welcome to the Color Store user { this.state.publicKey }!
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">ID</TableCell>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="center">RGB</TableCell>
+                <TableCell align="center">Owner</TableCell>
+                <TableCell align="center">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.colors.map((color) => (
+                <TableRow
+                  hover
+                  key={color.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="center">{color.id}</TableCell>
+                  <TableCell align="center">{color.name}</TableCell>
+                  <TableCell align="center">
+                    <CircleIcon sx={{ color: color.rgb }} />
+                  </TableCell>
+                  <TableCell align="center">&nbsp;</TableCell>
+                  <TableCell align="center">
+                    <Button variant="contained">View</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    );
+  };
 }
 
 export default App;
--- src/main/java/me/guligo/cs/controllers/ColorController.java
+++ src/main/java/me/guligo/cs/controllers/ColorController.java
@@ -3,6 +3,7 @@ package me.guligo.cs.controllers;
 import java.util.Arrays;
 import java.util.List;
 import me.guligo.cs.dtos.ColorDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ColorController {
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/colors")
     public List<ColorDto> getColors() {
         return Arrays.asList(
@@ -17,16 +19,24 @@ public class ColorController {
                         .id("1")
                         .name("Mimosa")
                         .rgb("#f0c05a")
+                        .build(),
+                ColorDto.builder()
+                        .id("2")
+                        .name("Greenery")
+                        .rgb("#88b04b")
                         .build());
     }
 
     @GetMapping("/colors/{colorId}")
     public ColorDto getColor(@PathVariable final String colorId) {
-        return ColorDto.builder()
-                .id("3")
-                .name("Mimosa")
-                .rgb("#f0c05a")
-                .build();
+        if ("1".equals(colorId)) {
+            return ColorDto.builder()
+                    .id("1")
+                    .name("Mimosa")
+                    .rgb("#f0c05a")
+                    .build();
+        }
+        return null;
     }
 
 }
