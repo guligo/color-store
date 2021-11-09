66a071c0f364ed94ef6304014dabe2052e03d004
guligo <igors.gulbinskis@gmail.com>
2021-11-09 21:30:24 +0100

    Send tokens to MM account on contract deployment

--- color-store-contract/contracts/ColorCoin.sol
+++ color-store-contract/contracts/ColorCoin.sol
@@ -5,8 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ColorCoin is ERC721 {
 
-    constructor() ERC721("ColorCoin", "COL") {
-    }
+    constructor() ERC721("ColorCoin", "COL") {}
 
     function createColor(address _to, uint256 _tokenId) public {
         super._mint(_to, _tokenId);
--- color-store-contract/migrations/2_deploy_contracts.js
+++ color-store-contract/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ColorCoin = artifacts.require("ColorCoin");
 
 module.exports = async function(deployer) {
-  deployer.deploy(ColorCoin);
+  await deployer.deploy(ColorCoin);
 
   let instance = await ColorCoin.deployed();
-  await instance.createColor("0x87939FB60BBa17210b840044BC380B98754af28d", 1); // Account 2 in MM
+  await instance.createColor("0x87939FB60BBa17210b840044BC380B98754af28d", 0xf0c05a); // Account 2 in MM
+  await instance.createColor("0x87939FB60BBa17210b840044BC380B98754af28d", 0x88b04b); // Account 2 in MM
+  await instance.createColor("0x87939FB60BBa17210b840044BC380B98754af28d", 0xdd4124); // Account 2 in MM
 };
