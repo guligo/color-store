f95f5058120b885b3102eff8e3bb2d83ddc1883b
guligo <igors.gulbinskis@gmail.com>
2021-11-09 20:49:22 +0100

    Mint ColorCoin on contract deployment

--- color-store-contract/migrations/2_deploy_contracts.js
+++ color-store-contract/migrations/2_deploy_contracts.js
@@ -2,4 +2,7 @@ const ColorCoin = artifacts.require("ColorCoin");
 
 module.exports = async function(deployer) {
   deployer.deploy(ColorCoin);
+
+  let instance = await ColorCoin.deployed();
+  await instance.createColor("0x87939FB60BBa17210b840044BC380B98754af28d", 1); // Account 2 in MM
 };
