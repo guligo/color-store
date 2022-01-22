fba1e0b695d3fa940bf176d945a4a5d691847583
guligo <igors.gulbinskis@gmail.com>
2022-01-22 11:58:01 +0100

    Add test for config controller

--- color-store-be/src/main/java/me/guligo/cs/dtos/ConfigDto.java
+++ color-store-be/src/main/java/me/guligo/cs/dtos/ConfigDto.java
@@ -1,16 +1,20 @@
 package me.guligo.cs.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfigDto {
 
     @lombok.NonNull
-    private final String colorCoinContractAddress;
+    private String colorCoinContractAddress;
 
     @lombok.NonNull
-    private final String colorStoreContractAddress;
+    private String colorStoreContractAddress;
 
 }
